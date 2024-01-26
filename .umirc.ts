16eb2ef1b9f9c621e04026ed900b5476095a79b7
Yshinu <yshinu2833@gmail.com>
2024-01-26 17:11:45 +0800

    mynav-init

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+  ],
+
+  npmClient: "yarn",
+  tailwindcss: {},
+  plugins: ["@umijs/plugins/dist/tailwindcss", "@umijs/plugins/dist/dva"],
+  dva: {},
+  codeSplitting: {
+    jsStrategy: 'granularChunks',
+  },
+
+});
